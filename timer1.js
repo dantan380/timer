143a32b163c86750b96c93797990fd7a256e86e6
Daniel Tan <danieltan380@gmail.com>
2023-09-26 18:21:41 -0700

    Linted

--- timer1.js
+++ timer1.js
@@ -1,22 +1,18 @@
 const args = process.argv.slice(2);
 
-for(time of args){
-    if (time < 0){
-        console.log("Please provide only positive numbers.");
-        process.exit();
-    } 
+for (let time of args) {
+  if (time < 0) {
+    console.log("Please provide only positive numbers.");
+    process.exit();
+  }
 
-    if (time === NaN ){
-        console.log("Please provide at least one positive number");
-        process.exit();
-    }
-
-    const regex = /^[^\d-]+$/g //Regex which includes any letter or symbol that is not an integer.
-    if (time.match(regex)){
-        console.log("Please do not provide letters or any other symbols. Only provide positive integers.")
-        process.exit();
-    }
-    setTimeout(() => {
-        process.stdout.write('\x07');
-    }, time * 1000)
-};
+  const regex = /^[^\d-]+$/g; //Regex which includes any letter or symbol that is not an integer.
+  if (time.match(regex)) {
+    console.log("Please do not provide letters or any other symbols. Only provide positive integers.");
+    process.exit();
+  }
+  
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, time * 1000);
+}
